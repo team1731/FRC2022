db66d5a6e28b6f1f418ba47075e9df0ce3b5f82b
Christian Benjamin <3.14159pii@gmail.com>
2022-02-22 17:59:18 -0500

    added code for first auto run

--- ELON_HUSK/src/main/java/frc/robot/autonomous/L4_B3L2_B5B4L2.java
+++ ELON_HUSK/src/main/java/frc/robot/autonomous/L4_B3L2_B5B4L2.java
@@ -9,10 +9,15 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.commands.intake.RightIntakeJoyconCommand;
+
+//import to create a new IntakeSubstem object
+import frc.robot.subsystems.IntakeSubsystem;
 
 /**
  * Starts at B3, launches 2, goes to B4 then B5 then launches 2
@@ -23,6 +28,8 @@ public class L4_B3L2_B5B4L2 extends _DelayableStrafingAutoMode {
 	private final String trajectory1JSON = AutoConstants.kPATH + ".wpilib.json";
 
 	private Pose2d _initPose;
+
+	private IntakeSubsystem m_intake;
 
 	@Override
 	public Pose2d getInitialPose() {
@@ -48,8 +55,15 @@ public class L4_B3L2_B5B4L2 extends _DelayableStrafingAutoMode {
 		m_robotDrive.resetOdometry(this._initPose);
 		SequentialCommandGroup commandGroup = new SequentialCommandGroup(
 			new WaitCommand(getInitialDelaySeconds()),
-			createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", 0, trajectory1)
+			createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", 0, trajectory1),
+
+			new ParallelCommandGroup(
+				new RightIntakeJoyconCommand(m_intake),
+
+				createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", 0, trajectory1)
+			)
 		);
+
 
 		command = commandGroup.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
 	}
