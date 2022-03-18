fdbae6c42758f62edb1f271e2b41c5ea76f6a205
Brent Lepke <bdleppke@gmail.com>
2022-03-18 18:07:01 -0400

    Autonomous - David
    - C2, C1, L4 changes
    - Tested and satisfied

--- ELON_HUSK/src/main/java/frc/robot/autonomous/L4_B3L2_B5B4L2.java
+++ ELON_HUSK/src/main/java/frc/robot/autonomous/L4_B3L2_B5B4L2.java
@@ -81,14 +81,14 @@ public class L4_B3L2_B5B4L2 extends _DelayableStrafingAutoMode {
 			new LaunchCommandStart(m_launch,0.55,true).raceWith(new RightIntakeCommand(m_intake)),
 			new LaunchCommandStart(m_launch,0.55,true).raceWith(createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", -35.0, trajectory0,false)), // Drive to first ball	
 			new LaunchBallCommandStart(m_launch),
-			new LaunchCommandStart(m_launch,0.55,true).withTimeout(1.5),
+			new LaunchCommandStart(m_launch,0.55,true).withTimeout(2.0),
 			new LaunchBallCommandStop(m_launch),
-			new LaunchCommandStart(m_launch,0.55,true).withTimeout(0.5), //spinup
-			new LaunchBallCommandStart(m_launch),
-			new LaunchCommandStart(m_launch,0.55,true).withTimeout(1),
-			new LaunchBallCommandStop(m_launch),
+			//new LaunchCommandStart(m_launch,0.55,true).withTimeout(0.5), //spinup
+			//new LaunchBallCommandStart(m_launch),
+			//new LaunchCommandStart(m_launch,0.55,true).withTimeout(1),
+			//new LaunchBallCommandStop(m_launch),
 			new LaunchCommandStart(m_launch,0.5,true).raceWith(createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", -40.0, trajectory1,false)),  // Drive to second ball
-	
+			new LaunchCommandStart(m_launch,0.55,true).withTimeout(1.0),
 			new LaunchCommandStart(m_launch,0.45,true).raceWith(createSwerveCommand(m_robotDrive, "L4_B3L2_B5B4L2", 0.0, trajectory2,false)),  // Drive to first ball				
 			new LaunchCommandStart(m_launch,0.46,true).raceWith(new RightStopCommand(m_intake)),
 			new LaunchBallCommandStart(m_launch),
